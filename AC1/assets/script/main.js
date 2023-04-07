eedd63d11068c469693df33c0447a37f019109ce
João melo <67669777+joao-java@users.noreply.github.com>
2023-04-07 13:53:18 -0300

    att

--- AC1/assets/script/main.js
+++ AC1/assets/script/main.js
@@ -1,12 +1,48 @@
-/*==================== PORTFOLIO SWIPER  ====================*/
-let swiperPortifolio = new Swiper('.slides', {
-    cssMode:true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEi: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+
+// ----------------- Slides
+const slides = document.querySelector('.slides');
+const slide = document.querySelectorAll('.slide');
+const intervalTime = 2000; // tempo em milissegundos
+
+let slideInterval = setInterval(nextSlide, intervalTime);
+
+function nextSlide() {
+  slides.scrollBy({
+    left: slide[0].offsetWidth,
+    behavior: 'smooth'
+  });
+
+  if (slides.scrollLeft + slides.offsetWidth >= slides.scrollWidth) {
+    slides.scroll({
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+}
+
+function prevSlide() {
+  slides.scrollBy({
+    left: -slide[0].offsetWidth,
+    behavior: 'smooth'
+  });
+
+  if (slides.scrollLeft === 0) {
+    slides.scroll({
+      left: slides.scrollWidth,
+      behavior: 'smooth'
+    });
+  }
+}
+
+function goToSlide(index) {
+  slides.scrollTo({
+    left: slide[index].offsetLeft,
+    behavior: 'smooth'
+  });
+}
+
+const btnAnterior = document.querySelector('.anterior');
+const btnProximo = document.querySelector('.proximo');
+
+btnAnterior.addEventListener('click', prevSlide);
+btnProximo.addEventListener('click', nextSlide);
